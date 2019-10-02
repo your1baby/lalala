b42038650d5035c8c3114a5304162a27092cbfca
1768561674@qq.com <wxl>
2019-10-02 21:10:23 +0800

    优惠券页面

--- src/app/meinfo/yhj.vue
+++ src/app/meinfo/yhj.vue
@@ -15,28 +15,23 @@
           </div>
 
           <!-- 优惠券卷样式 -->
-          <div class="coupon">
-            <div class="c-price">¥ 10</div>
+          <div class="coupon" v-for="(item,i) of coupon" :key="i">
+            <div class="c-price">¥ {{item.price}}</div>
             <ul class="c-text">
               <li>
-                <p>新用户红包</p>
-                <p>满149使用</p>
-                <p>使用期限2019-09-28 10:57至</p>
-                <p>2019-10-13 10:57</p>
+                <p class="c-text1">{{item.title}}</p>
+                <p class="c-text2">{{item.tb}}</p>
+                <p class="c-text2">{{item.datanew}}</p>
+                <p class="c-text2">{{item.data}}</p>
               </li>
             </ul>
           </div>
-          <div class="coupon">
-            <div class="c-price">¥ 10</div>
-            <ul class="c-text">
-              <li>
-                <p>新用户红包</p>
-                <p>满149使用</p>
-                <p>使用期限2019-09-28 10:57至</p>
-                <p>2019-10-13 10:57</p>
-              </li>
-            </ul>
-          </div>
+          
+        <!-- 底部样式 -->
+        <div class="yhjbottomm">
+          <p>没有更多可用的卷了</p>
+          <button>查看过期的卷</button>
+        </div>
   </div>
 </template>
 
@@ -44,7 +39,43 @@
 export default {
   data(){
     return{
-
+        coupon:[
+          {
+          price:10,
+          title:'新用户红包',
+          tb:"满149使用",
+          datanew:"使用期限2019-09-28 10:57至",
+          data:'2019-10-13 10:57'
+          },
+          {
+          price:10,
+          title:'新用户红包',
+          tb:"满149使用",
+          datanew:"使用期限2019-09-28 10:57至",
+          data:'2019-10-13 10:57'
+          },
+          {
+          price:10,
+          title:'新用户红包',
+          tb:"满149使用",
+          datanew:"使用期限2019-09-28 10:57至",
+          data:'2019-10-13 10:57'
+          },
+          {
+          price:10,
+          title:'新用户红包',
+          tb:"满149使用",
+          datanew:"使用期限2019-09-28 10:57至",
+          data:'2019-10-13 10:57'
+          },
+          {
+          price:10,
+          title:'新用户红包',
+          tb:"满149使用",
+          datanew:"使用期限2019-09-28 10:57至",
+          data:'2019-10-13 10:57'
+          },
+        ]
     }
   }
 }
@@ -56,6 +87,7 @@ export default {
    margin: 0;
  }
  .yhjbody{
+
    background:#f0f0f0;
  }
   /* 顶部导航栏重写样式 */
@@ -102,14 +134,47 @@ export default {
    background: #fff; 
   }
   .coupon .c-price{
-    border-right: 2px  dashed  #ddd;
+    border-right: 1px  dashed  #ddd;
     text-align: center;
     width: 30%;
     /* line-height: 100%; */
     /* height: 100%; */
     vertical-align:middle;
+    font-size: 30px;
+    margin-top:20px;
+    padding-top:8px;
+    margin-bottom:20px;
   }
   .coupon .c-text{
     width: 70%;
+    padding-left: 20px;
+    padding-top:10px;
+    padding-bottom: 8px;
+  }
+  .coupon  .c-text1{
+    padding-bottom: 5px;
+    font-weight: 500;
+  }
+  .coupon  .c-text2{
+    color:#b3aaaa;
+    font-size: 14px;
+    font-weight: 200;
+  }
+  /* 最底部样式 */
+  .yhjbottomm{
+    margin: 0 auto;
+    text-align: center;
+    padding-bottom: 20px;
+    color: #888;
+  }
+  .yhjbottomm p{
+    padding-bottom:10px;
+    
+  }
+  .yhjbottomm button{
+    background: #fff;
+    border-radius: 8px;
+    padding: 5px  10px;
+    border: 1px solid #ddd;
   }
 </style>
